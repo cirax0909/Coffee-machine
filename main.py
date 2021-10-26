03b64c24da5935407b4aba2ecd418a4d39f09676
cirax0909 <62638678+cirax0909@users.noreply.github.com>
2021-10-26 12:16:08 -0400

    version 1.0

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 from data import MENU
 from data import resources
+
 money = 0
-resource_water = resources['water']
-resource_milk = resources['milk']
-resource_coffee = resources['coffee']
 
 
-def check_resource(resource_water, resource_milk, resource_coffee, option):
+def check_resource(resources, option):
     if option == 'espresso':
-        if resource_water >= 50 and resource_coffee >= 18:
-            print('enough')
-        elif resource_water < 50 and resource_coffee >= 18:
+        if resources['water'] >= 50 and resources['coffee'] >= 18:
+            resources['water'] -= 50
+            resources['coffee'] -= 18
+            print('Here is your espresso ☕️. Enjoy!')
+        elif resources['water'] < 50 and resources['coffee'] >= 18:
             print('Sorry. Not enough water')
         else:
             print('Sorry. Not enough coffee')
     elif option == 'latte':
-        if resource_water >= 200 and resource_milk >= 150 and resource_coffee >= 24:
-            print('enough')
-        elif resource_water < 200 and resource_milk >= 150 and resource_coffee >= 24:
+        if resources['water'] >= 200 and resources['milk'] >= 150 and resources['coffee'] >= 24:
+            resources['water'] -= 200
+            resources['milk'] -= 150
+            resources['coffee'] -= 24
+            print('Here is your latte ☕️. Enjoy!')
+        elif resources['water'] < 200 and resources['milk'] >= 150 and resources['coffee'] >= 24:
             print('Sorry. Not enough water')
-        elif resource_water >= 200 and resource_milk < 150 and resource_coffee >= 24:
+        elif resources['water'] >= 200 and resources['milk'] < 150 and resources['coffee'] >= 24:
             print('Sorry. Not enough milk')
         else:
             print('Sorry. Not enough coffee')
     elif option == 'cappuccino':
-        if resource_water >= 250 and resource_milk >= 150 and resource_coffee >= 24:
-            print('enough')
-        elif resource_water < 200 and resource_milk >= 150 and resource_coffee >= 24:
+        if resources['water'] >= 250 and resources['milk'] >= 150 and resources['coffee'] >= 24:
+            resources['water'] -= 250
+            resources['milk'] -= 100
+            resources['coffee'] -= 24
+            print('Here is your cappuccino ☕️. Enjoy!')
+        elif resources['water'] < 200 and resources['milk'] >= 150 and resources['coffee'] >= 24:
             print('Sorry. Not enough water')
-        elif resource_water >= 200 and resource_milk < 150 and resource_coffee >= 24:
+        elif resources['water'] >= 200 and resources['milk'] < 150 and resources['coffee'] >= 24:
+            print('Sorry. Not enough milk')
+        elif resources['water'] >= 200 and resources['milk'] >= 150 and resources['coffee'] < 24:
             print('Sorry. Not enough milk')
         else:
-            print('Sorry. Not enough coffee')
+            print('Sorry. Not enough milk and water')
 
 
 def payment(cost):
@@ -45,28 +53,31 @@ def payment(cost):
     nickles_quan = int(input('how many nickles?:'))
     pennies_quan = int(input('how many pennies?:'))
     money_receive = float(quarter * quarter_quan + dimes_quan * dimes + nickles * nickles_quan + pennies * pennies_quan)
-    change = money_receive - cost
+    change = round(money_receive - cost, 2)
     if change < 0:
         print("Sorry that's not enough money. Money refunded.")
     else:
         print(f"Here is ${change} in change.")
+    return money_receive
 
 
 machine_on = True
 while machine_on:
     option = input("What would you like? (espresso/latte/cappuccino): ").lower()
-    check_resource(resource_water, resource_milk, resource_coffee, option)
     if option == 'off':
         machine_on = False
     elif option == 'report':
-        print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['water']}\n")
+        print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}\nMoney: {money}")
     elif option == 'espresso':
+        check_resource(resources, option)
         cost = MENU['espresso']['cost']
-        payment(cost)
+        money += payment(cost)
     elif option == 'latte':
+        check_resource(resources, option)
         cost = MENU['latte']['cost']
-        payment(cost)
+        money += payment(cost)
     elif option == 'cappuccino':
+        check_resource(resources, option)
         cost = MENU['cappuccino']['cost']
-        payment(cost)
+        money += payment(cost)
 
